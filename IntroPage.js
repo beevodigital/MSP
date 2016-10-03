4e9d72797bc13f43430388741ae8c142d6b853c8
brian.evans <brian.evans@brianevans.local>
2016-10-03 15:31:31 -0500

    initial intro layout

--- IntroPage.js
+++ IntroPage.js
@@ -48,35 +48,52 @@ class IntroPage extends React.Component{
   render() {
     return (
       <View style={styles.container}>
-        <Image source={require('./img/see28.jpg')} style={styles.backgroundImage} >
-          <View style={styles.halfcolumncontainer}>
+        <View style={styles.leftColumn}>
 
-            <View style={styles.columnRight, styles.whitebackground}>
-              <View style={styles.wrapText}>
-                <Text style={styles.mainFont}>
-                  Welcome to arts@msp's storyBooth, part of the see18 (multimedia|story|performance) installation.{"\n"}{"\n"}
-                </Text>
-                <Text style={styles.mainFont}>
-                  This space provides you the opportunity to record a 5 minute recollection of travel
-                </Text>
-                <TextInput
-                  ref={component => this._phoneNumber = component}
-                  style={styles.phonenumber}
-                  keyboardType="numeric"
-                  placeholder="Enter Phone Number and Press Return"
-                  placeholderTextColor="#666666"
-                  onChangeText={phoneNumber => this.setState({phoneNumber})}
-                  onSubmitEditing={this.navSecond.bind(this)}
-                  />
-
-                <Text style={styles.mainFont}>
-                  By leaving your story, arts@msp will retain the rights to curate your images and stories in future exhibits and for promotional purposes
-                </Text>
-              </View>
-
+            <View >
+              <Image source={require('./img/SplashIcons.png')}  style={styles.splashIcons}/>
             </View>
-          </View>
-        </Image>
+            <View>
+              <Text style={styles.boldHeadline}>
+                Hello!
+              </Text>
+              <Text style={styles.lightHeadline}>
+                !Hola!
+              </Text>
+            </View>
+            <View>
+              <Text style={styles.subHead}>Want to make you own shotfilm for See 18, MSP's film screening room?</Text>
+            </View>
+            <View>
+              <Image source={require('./img/fingerIcon.png')}  style={styles.fingerIcon}/>
+              <Text>
+                Tap Anywhere to start
+              </Text>
+            </View>
+            <View>
+              <Text>Legal Copy</Text>
+            </View>
+
+        </View>
+
+        <View style={styles.rightColumn}>
+          <View style={styles.wrapText}>
+            <Text style={styles.mainFont}>
+              This space provides you the opportunity to record a 5 minute recollection of travel
+            </Text>
+            <TextInput
+              ref={component => this._phoneNumber = component}
+              style={styles.phonenumber}
+              keyboardType="numeric"
+              placeholder="Enter Phone Number and Press Return"
+              placeholderTextColor="#666666"
+              onChangeText={phoneNumber => this.setState({phoneNumber})}
+              onSubmitEditing={this.navSecond.bind(this)}
+              />
+            </View>
+
+        </View>
+
 
       </View>
     );
@@ -90,43 +107,67 @@ class IntroPage extends React.Component{
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection:'row'
+    flexDirection:'row',
+    backgroundColor:'#3f85fc',
+    padding:25
     //backgroundColor: '#C9C9C9',
   },
-  mainFont:{
-    fontSize:30,
-    textAlign:'center'
+  leftColumn:{
+    //flexDirection:'row',
+    //alignItems: 'flex-end',
+    width:550
+  },
+  splashIcons:{
+    width:411,
+    height:182
+  },
+  boldHeadline:{
+    fontSize:130,
+    fontWeight:'bold'
+  },
+  lightHeadline:{
+    fontSize:130
+  },
+  subHead:{
+    fontSize:40
+  },
+  fingerIcon:{
+    width:53,
+    height:53
+  },
+  rightColumn:{
+    width:400
   },
   whitebackground:{
     backgroundColor: 'rgba(255,255,255,0.9)',
     height:740,
-    margin:10,
-    padding:10,
-    marginTop:20
+    //margin:10,
+    //padding:10,
+    //marginTop:20
   },
-  halfcolumncontainer:{
-    //flexDirection:'row',
-    alignItems: 'flex-end'
+
+  mainFont:{
+    fontSize:30,
+    textAlign:'center'
   },
+
+
   wrapText:{
-    flexDirection:'column',
-    width:600
+    backgroundColor: 'rgba(255,255,255,0.9)'
+    //flexDirection:'column',
+    //width:600
   },
-  columnRight:{
-    flex:1,
-    flexWrap:'wrap',
-    flexDirection:'row'
-  },
+
   phonenumberContainer:{
     //flex:1,
     backgroundColor:'#CCCCCC',
-    width:500,
+    //width:500,
     padding:25,
     margin:25,
     fontSize:20
   },
   phonenumber: {
-    width:600,
+    //width:600,
     height:70,
     borderColor: '#000000',
     borderWidth: 2,
@@ -141,12 +182,12 @@ var styles = StyleSheet.create({
     borderWidth:3
   },
   image: {
-    width: 100,
+    //width: 100,
     height: 100,
   },
   backgroundImage:{
-    flex:1,
-    resizeMode:'stretch'
+    //flex:1,
+    //resizeMode:'stretch'
   },
 
 });
